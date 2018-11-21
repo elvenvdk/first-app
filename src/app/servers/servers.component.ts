efbe1433608ac205c616a2dc24c0b4251b9933b5
Alvin.VanDeKuech <alvin.vandekuech@bjss.com>
2018-11-21 13:53:22 -0500

    Add updates

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  allowNewServer: boolean = false;
+  serverCreationStatus = 'No server was created.';
+  constructor() {
+    setTimeout(() => (this.allowNewServer = true), 2000);
+  }
 
   ngOnInit() {}
+
+  onCreateServer = () => {
+    this.serverCreationStatus = 'Server was created.';
+  };
 }
