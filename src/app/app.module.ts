d7f9475ad9ce05297c48ce630883310080c66a56
Alvin van der Kuech <alvin.vandekuech@bjss.com>
2018-11-21 06:27:47 -0500

    Add testcomponent plus modifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { TestcomponentComponent } from './testcomponent/testcomponent.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    TestcomponentComponent
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
